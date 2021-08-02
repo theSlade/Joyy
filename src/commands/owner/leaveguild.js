28549bf137b9a50ed4cb0c31eb7ee77da75ec026
LasseV12 <84160130+theSlade@users.noreply.github.com>
2021-08-02 20:27:21 +0200

    Lavede lidt på designet

--- src/commands/owner/leaveguild.js
+++ src/commands/owner/leaveguild.js
@@ -9,7 +9,7 @@ module.exports = {
    * @param {String[]} args
    */
   run: async (client, message, args) => {
-    const guild = client.guilds.cache.get("863417012867563550");
+    const guild = client.guilds.cache.get("822805220982915093");
     guild.leave().then(() => console.log(`Leaved: ${guild}`));
     message.reply(`Leaved: ${guild}`);
   },
